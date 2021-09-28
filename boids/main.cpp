eacb062377499bdb081ed26259f6f0bd88f19140
Jonas Wattendorff <jonas.wattendorff@gmail.com>
2021-09-28 13:17:53 +0200

    Initial Commit

---
+++ boids/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  boids
+//
+//  Created by Jonas Wattendorff on 28.09.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
